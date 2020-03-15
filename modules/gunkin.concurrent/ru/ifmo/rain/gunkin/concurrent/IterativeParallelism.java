a1aac98cd4e0b84700738ee5c96a80b7eb44b0ed
Anton Gunkin <ntngunkin@gmail.com>
2020-03-15 14:19:25 +0300

    Easy tests passed

--- modules/gunkin.concurrent/ru/ifmo/rain/gunkin/concurrent/IterativeParallelism.java
+++ modules/gunkin.concurrent/ru/ifmo/rain/gunkin/concurrent/IterativeParallelism.java
@@ -2,11 +2,14 @@ package ru.ifmo.rain.gunkin.concurrent;
 
 import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("OptionalGetWithoutIsPresent")
 public class IterativeParallelism implements ScalarIP {
     /**
      * Returns maximum value.
@@ -20,7 +23,40 @@ public class IterativeParallelism implements ScalarIP {
      */
     @Override
     public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return null;
+        List<List<? extends T>> parts = divideList(values, threads);
+        List<T> results = new ArrayList<>(parts.size());
+        List<Thread> workers = new ArrayList<>(parts.size());
+        for (int i = 0; i < parts.size(); i++) {
+            int finalI = i;
+            workers.add(new Thread(() -> {
+                List<? extends T> part = parts.get(finalI);
+                results.add(part.stream().max(comparator).get());
+            }));
+
+            workers.get(i).start();
+        }
+        for (int i =0;i< parts.size(); i++) {
+            workers.get(i).join();
+        }
+        return results.stream().max(comparator).get();
+    }
+
+    private <T> List<List<? extends T>> divideList(List<? extends T> list, int partsNumber) {
+        int step = list.size() / partsNumber;
+        int rest = list.size() % partsNumber;
+
+        List<List<? extends T>> parts = new ArrayList<>();
+        for (int l = 0; l < list.size(); ) {
+            int r = l + step;
+            if (rest > 0) {
+                r++;
+                rest--;
+            }
+            parts.add(list.subList(l, r));
+            l = r;
+        }
+
+        return parts;
     }
 
     /**
@@ -35,7 +71,22 @@ public class IterativeParallelism implements ScalarIP {
      */
     @Override
     public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return null;
+        List<List<? extends T>> parts = divideList(values, threads);
+        List<T> results = new ArrayList<>(parts.size());
+        List<Thread> workers = new ArrayList<>(parts.size());
+        for (int i = 0; i < parts.size(); i++) {
+            int finalI = i;
+            workers.add(new Thread(() -> {
+                List<? extends T> part = parts.get(finalI);
+                results.add(part.stream().min(comparator).get());
+            }));
+
+            workers.get(i).start();
+        }
+        for (int i =0;i< parts.size(); i++) {
+            workers.get(i).join();
+        }
+        return results.stream().min(comparator).get();
     }
 
     /**
@@ -49,7 +100,22 @@ public class IterativeParallelism implements ScalarIP {
      */
     @Override
     public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return false;
+        List<List<? extends T>> parts = divideList(values, threads);
+        List<Boolean> results = new ArrayList<>(parts.size());
+        List<Thread> workers = new ArrayList<>(parts.size());
+        for (int i = 0; i < parts.size(); i++) {
+            int finalI = i;
+            workers.add(new Thread(() -> {
+                List<? extends T> part = parts.get(finalI);
+                results.add(part.stream().allMatch(predicate));
+            }));
+
+            workers.get(i).start();
+        }
+        for (int i =0;i< parts.size(); i++) {
+            workers.get(i).join();
+        }
+        return results.stream().allMatch(b -> b);
     }
 
     /**
@@ -63,6 +129,21 @@ public class IterativeParallelism implements ScalarIP {
      */
     @Override
     public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return false;
+        List<List<? extends T>> parts = divideList(values, threads);
+        List<Boolean> results = new ArrayList<>(parts.size());
+        List<Thread> workers = new ArrayList<>(parts.size());
+        for (int i = 0; i < parts.size(); i++) {
+            int finalI = i;
+            workers.add(new Thread(() -> {
+                List<? extends T> part = parts.get(finalI);
+                results.add(part.stream().anyMatch(predicate));
+            }));
+
+            workers.get(i).start();
+        }
+        for (int i =0;i< parts.size(); i++) {
+            workers.get(i).join();
+        }
+        return results.stream().anyMatch(b -> b);
     }
 }
