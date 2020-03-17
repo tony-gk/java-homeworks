5eabecd00a9e16731711935dec89da862ba4ff5e
Anton Gunkin <ntngunkin@gmail.com>
2020-03-17 15:02:26 +0300

    mistakes fixed

--- modules/gunkin.concurrent/ru/ifmo/rain/gunkin/concurrent/package-info.java
+++ modules/gunkin.concurrent/ru/ifmo/rain/gunkin/concurrent/package-info.java
@@ -1,6 +1,5 @@
 /**
- * This package contains implementation of interface {@link info.kgeorgiy.java.advanced.concurrent.ListIP},
- * used for creating default implementation of classes and interfaces.
+ * This package contains implementation of interface {@link info.kgeorgiy.java.advanced.concurrent.ListIP}.
  */
 
 package ru.ifmo.rain.gunkin.concurrent;
