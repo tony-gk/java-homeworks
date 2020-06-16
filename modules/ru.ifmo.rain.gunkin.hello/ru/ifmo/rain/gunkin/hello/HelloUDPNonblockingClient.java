7e85a9005cda17490a577ff263f0261d0bb28904
Anton <ntngunkin@gmail.com>
2020-06-16 15:24:26 +0500

    code refactor

--- modules/ru.ifmo.rain.gunkin.hello/ru/ifmo/rain/gunkin/hello/HelloUDPNonblockingClient.java
+++ modules/ru.ifmo.rain.gunkin.hello/ru/ifmo/rain/gunkin/hello/HelloUDPNonblockingClient.java
@@ -30,143 +30,158 @@ public class HelloUDPNonblockingClient implements HelloClient {
             throw new IllegalArgumentException("Unknown host: " + host, e);
         }
 
-        Selector selector;
         try {
-            selector = Selector.open();
+            new RequestSender(prefix, channelCount, requestCount, socketAddress).start();
         } catch (IOException e) {
-            System.err.println("Failed to open selector");
             e.printStackTrace();
-            return;
         }
-        List<ChannelContext> channels = new ArrayList<>(channelCount);
+    }
 
-        for (int i = 0; i < channelCount; i++) {
-            try {
+    private static class RequestSender {
+        private final static int BYTE_BUFFER_SIZE = 1024;
+
+        private final String prefix;
+        private final int channelCount;
+        private final int requestCount;
+        private final ByteBuffer buffer;
+        private final List<ChannelContext> channelContexts;
+        private final Selector selector;
+        private final SocketAddress destination;
+
+        public RequestSender(String prefix, int channelCount, int requestCount, SocketAddress destination) throws IOException {
+            this.prefix = prefix;
+            this.channelCount = channelCount;
+            this.requestCount = requestCount;
+            this.buffer = ByteBuffer.allocate(BYTE_BUFFER_SIZE);
+            this.channelContexts = new ArrayList<>();
+            this.selector = Selector.open();
+            this.destination = destination;
+
+            for (int i = 0; i < channelCount; i++) {
                 DatagramChannel datagramChannel = DatagramChannel.open();
-                datagramChannel.connect(socketAddress);
                 datagramChannel.configureBlocking(false);
                 datagramChannel.register(selector, SelectionKey.OP_WRITE, i);
 
-                channels.add(new ChannelContext(datagramChannel, i));
-            } catch (IOException e) {
-                System.err.println("Failed to configure datagram channel");
-                e.printStackTrace();
-                return;
+                channelContexts.add(new ChannelContext(datagramChannel, i));
             }
         }
 
-        try {
-            startSelecting(selector, socketAddress, prefix, channelCount, requestCount, channels);
-        } catch (IOException e) {
-            System.err.println("IOException during selecting");
-            e.printStackTrace();
-        }
-    }
+        public void start() throws IOException {
+            int finishedCount = 0;
 
-    private void startSelecting(Selector selector, SocketAddress destination, String prefix,
-                                int channelCount, int requestCount, List<ChannelContext> channels) throws IOException {
-        int finishedCount = 0;
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (!Thread.interrupted() && finishedCount != channelCount) {
+                selector.select(TIMEOUT_MS);
 
-        while (!Thread.interrupted() && finishedCount != channelCount) {
-            selector.select(TIMEOUT_MS);
+                for (Iterator<SelectionKey> it = selector.selectedKeys().iterator(); it.hasNext(); ) {
+                    SelectionKey key = it.next();
+                    it.remove();
 
-            for (Iterator<SelectionKey> it = selector.selectedKeys().iterator(); it.hasNext(); ) {
-                SelectionKey key = it.next();
-                it.remove();
-
-                if (key.isWritable()) {
-                    try {
-                        sendRequest(key, destination, prefix, buffer, channels);
-                    } catch (IOException e) {
-                        System.err.println("IOException during sending");
-                        e.printStackTrace();
-                        continue;
-                    }
-                }
-                if (key.isReadable()) {
-                    try {
-                        boolean isLastResponse = receiveResponse(key, requestCount, buffer, channels);
-                        if (isLastResponse) {
-                            finishedCount++;
+                    if (key.isWritable()) {
+                        try {
+                            sendRequest(key);
+                        } catch (IOException e) {
+                            System.err.println("I/O error occurred during sending");
+                            e.printStackTrace();
+                            continue;
                         }
-                    } catch (IOException e) {
-                        System.err.println("IOException during receiving");
-                        e.printStackTrace();
+                    }
+                    if (key.isReadable()) {
+                        try {
+                            boolean isLastResponse = receiveResponse(key);
+                            if (isLastResponse) {
+                                finishedCount++;
+                            }
+                        } catch (IOException e) {
+                            System.err.println("I/O error occurred during receiving");
+                            e.printStackTrace();
+                        }
                     }
                 }
+
+                checkForTimeout();
             }
 
-            for (ChannelContext channelContext : channels) {
-                if (channelContext.isWaitingForRead &&
+            finish();
+        }
+
+        private void finish() throws IOException {
+            for (ChannelContext channelContext : channelContexts) {
+                channelContext.channel.close();
+            }
+            selector.close();
+        }
+
+        private void checkForTimeout() throws ClosedChannelException {
+            for (ChannelContext channelContext : channelContexts) {
+                if (channelContext.waitingForRead &&
                         System.currentTimeMillis() - channelContext.time > TIMEOUT_MS) {
                     channelContext.channel.register(selector, SelectionKey.OP_WRITE, channelContext.number);
-                    channelContext.isWaitingForRead = false;
+                    channelContext.waitingForRead = false;
                 }
             }
         }
-    }
 
-    private boolean receiveResponse(SelectionKey key, int requestCount, ByteBuffer buffer, List<ChannelContext> channels) throws IOException {
-        ChannelContext channelContext = channels.get((int) key.attachment());
-        DatagramChannel channel = channelContext.channel;
-        channelContext.isWaitingForRead = false;
+        private boolean receiveResponse(SelectionKey key) throws IOException {
+            ChannelContext channelContext = channelContexts.get((int) key.attachment());
+            DatagramChannel channel = channelContext.channel;
+            channelContext.waitingForRead = false;
 
-        buffer.clear();
-        channel.receive(buffer);
+            buffer.clear();
+            channel.receive(buffer);
 
-        buffer.flip();
-        String response = new String(buffer.array(), 0, buffer.limit(), StandardCharsets.UTF_8);
-        buffer.clear();
+            buffer.flip();
+            String response = new String(buffer.array(), 0, buffer.limit(), StandardCharsets.UTF_8);
 
-        if (ClientUtils.isValidEvilResponse(response, channelContext.number, channelContext.requestNumber)) {
-            System.out.println("Response: " + response + " --- correct");
-            channelContext.requestNumber++;
-        } else {
-            System.out.println("Response: " + response + " --- incorrect");
+            if (ClientUtils.isValidEvilResponse(response, channelContext.number, channelContext.requestNumber)) {
+                System.out.println("Response: " + response + " --- correct");
+                channelContext.requestNumber++;
+            } else {
+                System.out.println("Response: " + response + " --- incorrect");
+            }
+
+            if (channelContext.requestNumber < requestCount) {
+                channel.register(key.selector(), SelectionKey.OP_WRITE, channelContext.number);
+                return false;
+            } else {
+                return true;
+            }
         }
 
-        if (channelContext.requestNumber < requestCount) {
-            channel.register(key.selector(), SelectionKey.OP_WRITE, channelContext.number);
-            return false;
-        } else {
-            return true;
+        private void sendRequest(SelectionKey key) throws IOException {
+            ChannelContext channelContext = channelContexts.get((int) key.attachment());
+            DatagramChannel channel = channelContext.channel;
+
+            String request = prefix + channelContext.number + "_" + channelContext.requestNumber;
+
+            buffer.clear();
+            buffer.put(request.getBytes(StandardCharsets.UTF_8));
+            buffer.flip();
+
+            channel.send(buffer, destination);
+
+            System.out.println("Request: " + request);
+            channel.register(key.selector(), SelectionKey.OP_READ, channelContext.number);
+
+            channelContext.waitingForRead = true;
+            channelContext.time = System.currentTimeMillis();
         }
     }
 
-    private void sendRequest(SelectionKey key, SocketAddress destination, String prefix,
-                             ByteBuffer buffer, List<ChannelContext> channels) throws IOException {
-        ChannelContext channelContext = channels.get((int) key.attachment());
-        DatagramChannel channel = channelContext.channel;
 
-        String request = prefix + channelContext.number + "_" + channelContext.requestNumber;
-
-        buffer.clear();
-        buffer.put(request.getBytes(StandardCharsets.UTF_8));
-        buffer.flip();
-
-        channel.send(buffer, destination);
-
-        System.out.println("Request: " + request);
-        channel.register(key.selector(), SelectionKey.OP_READ, channelContext.number);
-
-        channelContext.isWaitingForRead = true;
-        channelContext.time = System.currentTimeMillis();
-    }
 
     private static class ChannelContext {
-        final DatagramChannel channel;
-        final int number;
-        int requestNumber;
-        long time;
-        boolean isWaitingForRead;
+        private final DatagramChannel channel;
+        private final int number;
+        private int requestNumber;
+        private  long time;
+        private boolean waitingForRead;
 
-        ChannelContext(DatagramChannel channel, int number) {
+        private ChannelContext(DatagramChannel channel, int number) {
             this.channel = channel;
             this.number = number;
             this.requestNumber = 0;
             this.time = System.currentTimeMillis();
-            this.isWaitingForRead = false;
+            this.waitingForRead = false;
         }
     }
 
